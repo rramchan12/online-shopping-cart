ff101a1675aca8fc6699c86f0db22367d882fee3
rramchan12 <ravi.ramchandran@gamil.com>
2020-10-03 02:26:40 +0530

    Fixed Readme, Added ER Diagrams and Implementation Notes

--- src/main/java/com/ravi/onlineshop/couponengine/SalesCouponCalculator.java
+++ src/main/java/com/ravi/onlineshop/couponengine/SalesCouponCalculator.java
@@ -8,10 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 @Slf4j
-public class SalesCouponDecorator extends BaseCouponCalculator {
+public class SalesCouponCalculator extends BaseCouponCalculator {
 
 
-    public SalesCouponDecorator(BaseOrderBo orderBo) {
+    public SalesCouponCalculator(BaseOrderBo orderBo) {
         super(orderBo);
     }
 
